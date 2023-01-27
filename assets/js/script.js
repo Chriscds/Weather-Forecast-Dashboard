1305fb41eda9e2735629b9581da139ce62479929
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-27 16:11:51 +0000

    Added ajax call and response in script.js. Added comments in index.html file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,40 @@
-// alert("loading");
-
 // Query URL variable
-var urlQuery = "";
-
+var urlQuery = "https://api.openweathermap.org/data/2.5/weather?";
 // Api key variable
-var apiKey = "50e333b7d0f2f6f179249a41b9ce7e72";
+var apiKey = "&appid=50e333b7d0f2f6f179249a41b9ce7e72";
+// empty variable to assign within the search function.
+var queryUrlAndKey;
 
-// funtion to search on click after input #search-input
-$("#search-input").on("click", function () {
 
-    
+
+// funtion to search on click after input #search-input.
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    // variable for search input value.
+    var searchInput = $("#search-input").val();
+    // queryUrlAndKey builds the search query.
+    queryUrlAndKey = urlQuery + "q=" + searchInput + apiKey;
+
+    // console the query.
+    console.log(queryUrlAndKey);
+
+    // ajax call function
+    $.ajax({
+        url: queryUrlAndKey,
+        method: "GET",
+    }).then(function (response) {
+
+    // Temperature convertion: Kelvin = - 273.15
+    var kelvin = response.main.temp;
+    var celsius = kelvin -273.15; // Need to change result in to 2 digits after decimal
+
+    // log url query
+    console.log(celsius + " C");
+
+        // log response
+
+        // content to transfer to HTML
+
+    })
+
 });
-
-
-
-// ajax call function
-$.ajax({
-    url: urlQuery,
-    method: "GET",
-}).then(function(response) {
-
-// log url query
-
-// log response
-
-// content to transfer to HTML
-
-})
