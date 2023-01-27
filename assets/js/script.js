14e8c1cee10f1f5159acf9477f61447e67953b15
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-27 15:29:37 +0000

    Building function in script.js. Added txt files to project.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 // alert("loading");
 
 // Query URL variable
+var urlQuery = "";
 
 // Api key variable
+var apiKey = "50e333b7d0f2f6f179249a41b9ce7e72";
+
+// funtion to search on click after input #search-input
+$("#search-input").on("click", function () {
+
+    
+});
+
+
 
 // ajax call function
+$.ajax({
+    url: urlQuery,
+    method: "GET",
+}).then(function(response) {
 
 // log url query
 
@@ -12,3 +26,4 @@
 
 // content to transfer to HTML
 
+})
