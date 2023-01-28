7c173ea2eaef20f4bf02e0e97fbf03c304b90400
CDS-admin1 <93998859+CDS-admin1@users.noreply.github.com>
2023-01-28 19:20:36 +0000

    Response for weather in script.js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,13 +34,11 @@ $("#search-button").on("click", function (event) {
     // Console logs the result of celsius with only two didgets after the decimal.
     console.log(celsius.toFixed(2) + " C");
 
-        // log response
-
-// Content to transfer to HTML
+// Content to transfer to HTML ----------------------------------------------------------------
 
     // create div to display the results of the search query/variables below 
     // to prepend to ID #today section
-        var todaysWeather = $('<div class="current-weather">');
+    var todaysWeather = $('<div class="current-weather">');
 
     // variables for different results to store:
 
@@ -58,14 +56,18 @@ $("#search-button").on("click", function (event) {
 
     // Temperature (p)
     // temperature is coming from variable ln 29 var celsius = kelvin -273.15;
-    var tempToday = $('<p>').text("Temp: " + celsius.toFixed(2) + degreesSymbol + " C");
+    var tempToday = $('<p>').text("Temp: " + celsius.toFixed(2) + " " + degreesSymbol + "C");
     todaysWeather.append(tempToday);
 
     // Wind (p)
-    // var windSpeed = response.;
+    var windSpeed = response.wind.speed;
+    var windToday = $('<p>').text("Wind speed: " + windSpeed + " kph"); // check if kph is the correct value
+    todaysWeather.append(windToday);
 
     // Humidity (p)
-    // var humidity = response.;
+    var humidity = response.main.humidity;
+    var humidityToday = $('<p>').text("Humidity: " + humidity + " %");
+    todaysWeather.append(humidityToday);
 
 // Prepend to #today 
         $("#today").prepend(todaysWeather);
